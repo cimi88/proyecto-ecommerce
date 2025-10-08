9c6d204fe6a5203c38fb6ac5d146a44e6fb6b97e
cimi88 <salgueiroezequieli@gmail.com>
2025-10-07 22:59:08 -0300

    Preparando para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/proyecto-ecommerce/">
       <App />
     </BrowserRouter>
   </StrictMode>,
